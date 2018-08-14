86ebd8cf204e521c80721a63b32b2533db4017ba
labAxiaoming <34019940+labAxiaoming@users.noreply.github.com>
2018-08-14 17:06:21 +0800

    上传文件

---
+++ register.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 26 15:54:13 2018
+
+@author: ssiwo
+"""
+import face_recognition
+import time
+import cv2
+import numpy as np
+cap=cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_DUPLEX 
+name=input('registering,please input your name:')   
+i=-20
+while True:
+    time1=time.time()
+    ret,img=cap.read()
+    if ret ==1:
+#        fps=1/(time.time()-time1)
+#        cv2.putText(img,'fps:%0.1f'%fps,(20,20),font,1.0,(255,255,0),1)
+        
+        cv2.imshow('img',img)
+        ll=face_recognition.face_encodings(img)
+        if len(ll)==1:
+            face_locations = face_recognition.face_locations(img)
+            top,right,bottom,left=face_locations[0]
+            face0=img[top:bottom,left:right]
+#            cv2.imshow('img',face0)
+            if i>-1:
+                cv2.imwrite('know/%s_%d.jpg'%(name,i),face0)
+        i+=1
+        if i>4:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+#cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()  
